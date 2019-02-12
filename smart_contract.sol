7388dd497e5da3d3435f542113664e09734cf4a7
charlie-horn <charlie.horn@queensu.ca>
2019-02-11 21:50:48 -0500

    Initial commit for Module 3

---
+++ smart_contract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+
+contract coin.ico{
+    
+    // Total number of coin
+    uint public max_coins = 1000000;
+    
+    // Conversion rate 
+    uint public coins_per_dollar = 1000;
+    
+    // Total number of coin bought
+    uint public coins_bought = 0;
+    
+    // Equity mappings
+    mapping(address => uint) equity_coin;
+    mapping(address => uint) equity_dollar;
+}
